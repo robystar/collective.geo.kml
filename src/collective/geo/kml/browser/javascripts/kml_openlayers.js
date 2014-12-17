10baf2052407d0462983b240bac4b0747e201458
Roberto <roberto.starnini@gmail.com>
2014-12-17 11:21:03 +0100

    bugfix

--- src/collective/geo/kml/browser/javascripts/kml_openlayers.js
+++ src/collective/geo/kml/browser/javascripts/kml_openlayers.js
@@ -3,6 +3,8 @@
 (function ($) {
     "use strict";
 
+
+  $(function () {  
     Proj4js.defs["EPSG:3003"] = "+proj=tmerc +lat_0=0 +lon_0=9 +k=0.999600 +x_0=1500000 +y_0=0 +ellps=intl +units=m +no_defs +towgs84=-104.1,-49.1,-9.9,0.971,-2.917,0.714,-11.68";
     var epsg3003 = new OpenLayers.Projection('EPSG:3003');
 
@@ -186,7 +188,8 @@
     error: function (e) { 
             console.log(e)
     }
-})
+ });
+});
 
 
 }(jQuery));
